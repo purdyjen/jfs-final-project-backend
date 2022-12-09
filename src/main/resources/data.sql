2a5e2c3f0a5dc24f7f7453e3f1926110ea814263
purdyjen <purdyjen@me.com>
2022-12-09 13:57:04 -0500

    finished task 8

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO product (name, description, image_url, image_alt_text, price)
+VALUES ("Brand Name Cat Food", "Don't forget to feed your precious fur babies.", "https://cdn.pixabay.com/photo/2017/03/24/08/27/cat-2170495_1280.jpg", "A white bowl with cute cat ears full of cat food", 15.99);
+
+INSERT INTO product (name, description, image_url, image_alt_text, price)
+VALUES ("Brand Name Dog Food", "Because dogs are ALWAYS starving", "https://images.unsplash.com/photo-1589924691995-400dc9ecc119?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2371&q=80", "A white bowl with green edges full of dog food", 15.99);
+
+INSERT INTO product (name, description, image_url, image_alt_text, price)
+VALUES ("Brand Name Dog Treats", "Who's a good boy?? You're a good boy!", "https://cdn.pixabay.com/photo/2014/05/21/18/08/dog-bones-350092_1280.jpg", "A colorful variety of bone-shaped dog treats", 10.99);
